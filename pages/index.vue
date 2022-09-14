1b75baf41e6bc110bcdb299e9ef018033092250c
Rafal Chlodnicki <rchl2k@gmail.com>
2022-09-14 23:53:35 +0200

    module-resolution: node and aria attribute

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,9 @@
 <template>
-  <div>
-    {{ aprop  }}
-    {{ foo  }}
-    {{ callMeAndGetAString(1) }}
-    <tutorial :test="'asdfds'" />
-    <nuxt-logo></nuxt-logo>
-  </div>
+    <div aria-live="polite"></div>
 </template>
 
 <script>
 export default {
-  props: {
-    aprop: {
-      type: Number,
-    }
-  },
-  async asyncData(context) {
-    context.redirect('https://google.com');
-  },
-  data() {
-    return {
-      foo: 'foo'
-    }
-  },
-  // Uncommenting the following object makes template types work.
-  // computed: {
-  //   xxx() {
-  //     return true;
-  //   },
-  // },
-  methods: {
-    /**
-     * @param {number} arg
-     * @return {string}
-     */
-    callMeAndGetAString(arg) {
-      return String(arg) + 'xxx';
-    }
-  },
-  mounted() {
-    this.foo // string
-  }
+    name: 'Index',
 }
 </script>

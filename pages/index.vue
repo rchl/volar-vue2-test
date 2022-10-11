82cc5f1009a49d49cc0825d210484f17ea404e93
Rafal Chlodnicki <rchl2k@gmail.com>
2022-10-11 21:27:14 +0200

    add prop for testing jsdoc

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div aria-live="polite">
     <div class="some-class">{{ computedProp }}</div>
+    <div>{{ items }}</div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Index',
+  props: {
+    /** @type {import('vue').PropOptions<number[]>} */
+    items: {
+      type: Array,
+      default: () => [1, 2, 3]
+    }
+  },
   computed: {
     computedProp() {
       return 'foo'

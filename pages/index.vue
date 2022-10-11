0407ac8f14391aa41d5e5afbc4f182baabd54cb8
Rafal Chlodnicki <rchl2k@gmail.com>
2022-10-11 22:59:08 +0200

    Vue 2.7

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div aria-live="polite">
-    <div class="some-class">{{ computedProp }}</div>
     <div>{{ items }}</div>
   </div>
 </template>
@@ -15,16 +14,5 @@ export default {
       default: () => [1, 2, 3]
     }
   },
-  computed: {
-    computedProp() {
-      return 'foo'
-    },
-  },
 }
 </script>
-
-<style scoped>
-.some-class {
-  color: blue;
-}
-</style>
